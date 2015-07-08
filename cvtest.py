985b032d7dc20d1c7d6a812d138b53e7c95baa1f
Niwa Toshihiro <oniwanko28@gmail.com>
2015-07-08 16:46:31 +0900

    Update cvtest.py

--- cvtest.py
+++ cvtest.py
@@ -3,7 +3,7 @@
 import cv2
 
 def main():
-    gray = cv2.imread("img/tes2.jpg",0)     # 入力画像をグレースケールで取得
+    gray = cv2.imread("test.jpg",0)     # 入力画像をグレースケールで取得
     edge = cv2.Canny(gray, 100, 200)    # Cannyアルゴリズムでエッジ検出
     cv2.imshow("Show Image",edge)       # エッジ画像を表示
     cv2.waitKey(0)                      # キー入力待機
